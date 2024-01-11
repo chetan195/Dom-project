1caf97c6a58528704a754d6c8460612cc1935d75
chetan195 <102486446+chetan195@users.noreply.github.com>
2024-01-11 20:43:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const div = document.getElementById("root");
+
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
